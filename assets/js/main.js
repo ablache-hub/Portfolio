df6b5605c4ef6dcdc86fbe42aa7607f882c7e627
ablache <alexandreblache.pro@gmail.com>
2021-12-06 18:18:26 +0100

    popup

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,23 +1,23 @@
 /*===== MENU SHOW Y HIDDEN =====*/
 const toggleMenu = document.getElementById('nav-toggle'),
-navMenu = document.getElementById('nav-menu'),
-closeMenu = document.getElementById('nav-close')
+    navMenu = document.getElementById('nav-menu'),
+    closeMenu = document.getElementById('nav-close')
 
 
 // SHOW
-toggleMenu.addEventListener('click', ()=>{
+toggleMenu.addEventListener('click', () => {
     navMenu.classList.toggle('show')
 })
 
 // HIDDEN
-closeMenu.addEventListener('click', ()=>{
+closeMenu.addEventListener('click', () => {
     navMenu.classList.remove('show')
 })
 
 /*===== FERME MENU AU CLICK =====*/
 const navLink = document.querySelectorAll('.nav__link')
 
-function linkAction(){
+function linkAction() {
     navMenu.classList.remove('show')
 }
 
@@ -25,15 +25,29 @@ navLink.forEach(n => n.addEventListener('click', linkAction))
 
 //--------------------------------------------------------------------------------------------------------------
 // Icone menu
-const togglePopup = document.getElementById('popup-toggle'),
-popupWrapper = document.getElementById('popup-wrapper'),
-popupClose = document.getElementById('popup-close')
+// const togglePopup = document.getElementById('popup-toggle'),
+// popupWrapper = document.getElementById('popup-wrapper'),
+// popupClose = document.getElementById('popup-close')
 
-togglePopup.addEventListener('click', ()=>{
-    popupWrapper.classList.toggle('toggleShow')
-})
+// togglePopup.addEventListener('click', ()=>{
+//     popupWrapper.classList.toggle('toggleShow')
+// })
 
-// HIDDEN
-popupClose.addEventListener('click', ()=>{
-    popupWrapper.classList.remove('toggleShow')
-})
+// // HIDDEN
+// popupClose.addEventListener('click', ()=>{
+//     popupWrapper.classList.remove('toggleShow')
+// })
+
+function displayPopup(id) {
+    var projectId = id.charAt(id.length - 1)
+    var popup = document.getElementById("popup-wrapper-" + projectId)
+    var style = popup.style.display
+    console.log(projectId, popup, style)
+
+    if (popup.style.display === "none" || !popup.style.display) {
+        popup.style.display = "block";
+    } else {
+        popup.style.display = "none";
+    }
+}
+
